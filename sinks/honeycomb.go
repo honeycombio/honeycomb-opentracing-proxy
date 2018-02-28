09574c1586f911ccbc82723c9f7eb12cced045c4
Eben Freeman <eben@honeycomb.io>
2018-02-27 19:28:12 -0800

    Propagate sample rate in honeycomb events
    Similarly to #8, we look for a `honeycomb.samplerate` tag, and use its
    value as the sample rate when sending events to Honeycomb. Note that the
    proxy doesn't do any *additional* sampling itself; it still forwards all the
    spans it gets.
    
    Test plan: unit tests

--- sinks/honeycomb.go
+++ sinks/honeycomb.go
@@ -9,6 +9,7 @@ import (
 )
 
 const datasetKey = "honeycomb.dataset"
+const sampleRateKey = "honeycomb.samplerate"
 
 // HoneycombSink implements the Sink interface. It sends spans to the Honeycomb
 // API.
@@ -45,23 +46,54 @@ spanLoop:
 		ev.Timestamp = s.Timestamp
 		ev.Add(s.CoreSpanMetadata)
 		for k, v := range s.BinaryAnnotations {
-			if k == datasetKey {
+			switch k {
+			case datasetKey:
 				// Let clients route spans to different datasets using the
 				// `honeycomb.dataset` tag.
-				ds, ok := v.(string)
-				if !ok {
-					logrus.WithField("honeycomb.dataset", v).Error("unexpected type for honeycomb.dataset tag value")
+				if ds, ok := v.(string); ok {
+					ev.Dataset = ds
+				} else {
+					logrus.WithField("honeycomb.dataset", v).Error(
+						"unexpected type for honeycomb.dataset tag value")
 					continue spanLoop
 				}
-				ev.Dataset = ds
-			} else {
+			case sampleRateKey:
+				if sampleRate, ok := extractUint(v); ok {
+					ev.SampleRate = sampleRate
+				} else {
+					logrus.WithField(sampleRateKey, v).Error(
+						"unexpected value for honeycomb.samplerate tag")
+					// Let's not drop on invalid sample rate though
+				}
+			default:
 				ev.AddField(k, v)
 			}
 		}
-		err := ev.Send()
+		err := ev.SendPresampled()
 		if err != nil {
 			logrus.WithError(err).Info("Error sending libhoney event")
 		}
 	}
 	return nil
+}
+
+// Extract an unsigned int from an interface{} type if possible, so that we can
+// get a samplerate value from a span tag.
+// This implementation relies on us having converted annotation values of string
+// type to int64 or float64 values in guessAnnotationType().
+func extractUint(v interface{}) (uint, bool) {
+	switch val := v.(type) {
+	case int64:
+		if val < 0 {
+			return 0, false
+		}
+		return uint(val), true
+	case float64:
+		if val < 0 {
+			return 0, false
+		}
+		return uint(val), true
+	default:
+		return 0, false
+	}
 }
